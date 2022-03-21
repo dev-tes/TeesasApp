ece3ddd15e1e6ce6b25a9332104f6c8c8e0eeb10
dev-tes <tesleem.amuda@gmail.com>
2022-03-21 22:26:06 +0100

    create labels of the login view controller

--- TeesasApp/TeesasApp/ViewControllers/LoginViewController.swift
+++ TeesasApp/TeesasApp/ViewControllers/LoginViewController.swift
@@ -9,6 +9,35 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    let LoginLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Login"
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        return label
+    }()
+    
+    let footerLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Great to see you again!"
+        label.font = UIFont.systemFont(ofSize: 15)
+        return label
+    }()
+    
+    let phoneNumberTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.black.cgColor
+        textField.layer.cornerRadius = 25
+        textField.layer.backgroundColor = UIColor.white.cgColor
+        textField.setPadding(left: 20, right: 20)
+        textField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+        return textField
+    }()
+    
     let passwordTextField: UITextField = {
         let textField = UITextField()
         textField.translatesAutoresizingMaskIntoConstraints = false
@@ -18,12 +47,27 @@ class LoginViewController: UIViewController {
         textField.layer.backgroundColor = UIColor.white.cgColor
         textField.setPadding(left: 20, right: 48)
         textField.isSecureTextEntry = true
-        textField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        textField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
         return textField
     }()
-
+    
+    let checkBoxButton: UIButton = {
+        let checkbox = UIButton.init(type: .custom)
+        checkbox.setImage(UIImage.init(named: "iconCheckboxOutlined"), for: .normal)
+        checkbox.setImage(UIImage.init(named: "iconCheckboxFilled"), for: .selected)
+        checkbox.addTarget(self, action: #selector(toggleCheckboxSelection), for: .touchUpInside)
+        return checkbox
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+    }
+    
+    @objc func textFieldDidChange() {
+    }
+    
+    @objc func toggleCheckboxSelection() {
+        checkBoxButton.isSelected = !checkBoxButton.isSelected
     }
 }
--- TeesasApp/TeesasApp/ViewControllers/RegisterViewController.swift
+++ TeesasApp/TeesasApp/ViewControllers/RegisterViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
+    
+    private let backButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(systemName: "arrow.left"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(backButtonPressed), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
+    }
+    
+    @objc func backButtonPressed() {
+        navigationController?.popViewController(animated: true)
     }
 }
 
