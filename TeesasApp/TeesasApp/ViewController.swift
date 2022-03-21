aa01f268f26f844b2e175469b2555155943d6bb5
dev-tes <tesleem.amuda@gmail.com>
2022-03-21 17:40:52 +0100

    create starter project

---
+++ TeesasApp/TeesasApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  TeesasApp
+//
+//  Created by Tes on 21/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
