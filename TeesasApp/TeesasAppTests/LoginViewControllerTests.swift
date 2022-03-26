0b2896d03706fe62661a0ffeac8e4ac57219f3f3
dev-tes <tesleem.amuda@gmail.com>
2022-03-26 17:30:39 +0100

    test login button configuration

--- TeesasApp/TeesasAppTests/LoginViewControllerTests.swift
+++ TeesasApp/TeesasAppTests/LoginViewControllerTests.swift
@@ -11,26 +11,29 @@ import XCTest
 class LoginViewControllerTests: XCTestCase {
     
     var sut: LoginViewController!
-
+    
     override func setUpWithError() throws {
         try super.setUpWithError()
         sut = LoginViewController()
     }
-
+    
     override func tearDownWithError() throws {
         sut = nil
         try super.tearDownWithError()
     }
-
-    func testIfLoginButtonIsGreen(){
+    
+    func testIfLoginButtonConfiguration(){
         // given
         let guess = sut.loginButton
         
         // when
         sut.loadViewIfNeeded()
-
+        
         // then
         XCTAssertNotNil(guess)
+        XCTAssertEqual(guess.layer.borderColor, UIColor.systemGreen.cgColor)
+        XCTAssertFalse(guess.translatesAutoresizingMaskIntoConstraints)
+        XCTAssertEqual(guess.titleLabel?.text, "Login")
     }
-
+    
 }
